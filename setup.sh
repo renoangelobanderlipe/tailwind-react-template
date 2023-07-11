506008e62986fa73762e64070678c52d1debd107
Reno Angelo Banderlipe <radb@newmediastaff.com>
2023-07-11 17:21:12 +0800

    added additional configurations

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ echo "Enter Your Template Name: "
 
 read template_name
 
+echo "Enter Your Name: "
+
+read project_name
+
 npm create vite@latest ${template_name} -- --template react
 
 cd ${template_name}
@@ -11,6 +15,30 @@ cd ${template_name}
 npm install -D tailwindcss postcss autoprefixer
 
 npx tailwindcss init -p
+
+cd ./public
+
+mkdir images icons
+
+cd ../
+
+cat <<EOT > index.html
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="images/icon-logo.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>${project_name}</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
+
+EOT
 
 cat <<EOT > tailwind.config.js
 
@@ -21,12 +49,176 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens : {
+      'phone' : '375px',
+      'phone-lg' : '768px',
+      'tablet' : '1024px',
+      'laptop' : '1280px',
+      'laptop-lg' : '1440px',
+      'desktop' : '1728px',
+    },
+    padding : {
+      '200' : '200px',
+      '100' : '100px',
+      '64' : '64px',
+      '48' : '48px',
+      '24' : '24px',
+      '20' : '20px',
+      '16' : '16px',
+      '8' : '8px',
+    },
+    gap : {
+      '100' : '100px',
+      '64' : '64px',
+      '48' : '48px',
+      '24' : '24px',
+      '16' : '16px',
+      '8' : '8px',
+    },
+    height: {
+      'button': '56px',
+      'button-m' : '40px',
+    },
+    borderRadius: {
+      'none': '0',
+      'button': '30px',
+      'card': '20px'
+    },
+    fontSize : {
+      'h1' : ['120px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '700',
+      }],
+      'h2' : ['64px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'h3' : ['48px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'h4' : ['36px', {
+        lineHeight: 'auto',
+        letterSpacing: '9.18px',
+        fontWeight: '600',
+      }],
+      'h5' : ['24px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '700',
+      }],
+      'body' : ['20px', {
+        lineHeight: '1.5em',
+        letterSpacing: 'auto',
+        fontWeight: '400',
+      }],
+      'subheading' : ['20px', {
+        lineHeight: 'auto',
+        letterSpacing: '6.2px',
+        fontWeight: 'normal',
+      }],
+      'tablet-h1' : ['75px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '700',
+      }],
+      'tablet-h2' : ['40px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'tablet-h3' : ['32px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'tablet-h4' : ['20px', {
+        lineHeight: 'auto',
+        letterSpacing: '5.1px',
+        fontWeight: '600',
+      }],
+      'tablet-h5' : ['20px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '700',
+      }],
+      'tablet-subhead' : ['18px', {
+        lineHeight: 'auto',
+        letterSpacing: '5.58px',
+        fontWeight: '600',
+      }],
+      'tablet-body' : ['16px', {
+        lineHeight: '1.5em',
+        letterSpacing: 'auto',
+        fontWeight: 'normal',
+      }],
+      'phone-h1' : ['45px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '700',
+      }],
+      'phone-h2' : ['36px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'phone-h3' : ['32px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'phone-h4' : ['20px', {
+        lineHeight: 'auto',
+        letterSpacing: '5.1px',
+        fontWeight: '600',
+      }],
+      'phone-h5' : ['20px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'phone-h6' : ['18px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '600',
+      }],
+      'phone-subhead' : ['14px', {
+        lineHeight: 'normal',
+        letterSpacing: '4.34px',
+        fontWeight: '600',
+      }],
+      'phone-body' : ['16px', {
+        lineHeight: '1.5em',
+        fontWeight: '400',
+      }],
+      'btn-lg' : ['18px', {
+        lineHeight: '26px',
+        letterSpacing: '0.46px',
+        fontWeight: '700',
+      }],
+      'btn-m' : ['14px', {
+        lineHeight: '26px',
+        letterSpacing: '0.46px',
+        fontWeight: '700',
+      }],
+      'email' : ['16px', {
+        lineHeight: 'auto',
+        letterSpacing: 'auto',
+        fontWeight: '700',
+        fontFamily : 'Courier New',
+      }],
+    },
     extend: {},
   },
   plugins: [],
 }
 
 EOT
+
+rm -rf $(pwd)/public/vite.svg
 
 cd ./src
 
