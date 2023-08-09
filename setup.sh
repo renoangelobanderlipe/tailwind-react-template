704c7cf12f94157a358a0349c22b7e629075f9b4
Reno Angelo <personal.renoangelo@gmail.com>
2023-08-09 21:37:45 +0800

    refs #1 added import css file to read tailwind properties closes #1

--- setup.sh
+++ setup.sh
@@ -270,6 +270,7 @@ cat <<EOT > main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { PAGES } from './routes';
+import index from './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
